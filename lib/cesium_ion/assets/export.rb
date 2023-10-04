76a800c1df9e0e6f628f2e4bcfc41515b52cc8ca
Saurabh gulati <saurabhgulati159@gmail.com>
2023-10-05 04:39:38 +0530

    Fixed export API

--- lib/cesium_ion/assets/export.rb
+++ lib/cesium_ion/assets/export.rb
@@ -3,8 +3,7 @@ module CesiumIon
     class Export < CesiumIon::Base
 
       Params = Struct.new(
-        :inspection_id,
-        :site_model_id,
+        :prefix,
         :asset_id,
         keyword_init: true
       )
@@ -24,15 +23,14 @@ module CesiumIon
         {
           "type": "S3",
           "bucket": CesiumIon.configuration.bucket,
-          "prefix": "#{Rails.env}/inspection/#{@params.inspection_id}/site_model/#{@params.site_model_id}",
+          "prefix": @params.prefix,
           "accessKeyId": CesiumIon.configuration.access_key_id,
           "secretAccessKey": CesiumIon.configuration.secret_access_key
         }
       end
 
       def validate
-        @errors['site_model_id'] << 'Can\'t be blank' if @params.site_model_id.to_s.empty?
-        @errors['inspection_id'] << 'Can\'t be blank' if @params.inspection_id.to_s.empty?
+        @errors['prefix'] << 'Can\'t be blank' if @params.prefix.to_s.empty?
         @errors['asset_id'] << 'Can\'t be blank' if @params.asset_id.to_s.empty?
       end
 
--- lib/cesium_ion/version.rb
+++ lib/cesium_ion/version.rb
@@ -1,3 +1,3 @@
 module CesiumIon
-  VERSION = '0.1.1'
+  VERSION = '0.1.2'
 end
